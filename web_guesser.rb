4ec3c412a3ee40a062d8bfbb40e738d448f75f3f
brian <lbrian357@gmail.com>
2016-09-28 20:30:33 +0300

    I4: background color change depending on guess feature added

--- web_guesser.rb
+++ web_guesser.rb
@@ -5,7 +5,18 @@ number = rand(101)
 get '/' do
   guess = params['guess'].to_i 
   message = check_guess(guess, number)
-  erb :index, :locals => {:number => number, :message => message} #saves number to locals hash
+  b_color = change_color(message)
+  erb :index, :locals => {:number => number, :message => message, :b_color => b_color} #saves number to locals hash
+end
+
+def change_color(string)
+  if string == 'Way too high!' || string == 'Way too low!'
+    return 'background: red;'
+  elsif string == 'Too high!' || string == 'Too low!'
+    return 'background: #ff5050;'
+  elsif string[0..6] == 'You got'
+    return 'background: green'
+  end
 end
 
 def check_guess(guess, number)
@@ -19,7 +30,7 @@ def check_guess(guess, number)
     if guess < (number - 5)
       return 'Way too low!'
     else
-      return 'Too Low!'
+      return 'Too low!'
     end
   else
     return "You got it right! The SECRET NUMBER is #{number}"
